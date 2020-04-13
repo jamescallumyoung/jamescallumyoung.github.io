f357c21c6723e49a469189d782c68b12c23b5097
James Young <hello@jamesyoung.ch>
2020-04-13 15:53:10 +0200

    Updates

---
+++ precache-manifest.126982f4d472f846d7c0100e0b06f745.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad88b2e71a7050fa5391abe5ff714e2c",
+    "url": "/jamesyoung.ch/index.html"
+  },
+  {
+    "revision": "19607ebcf114d58905bb",
+    "url": "/jamesyoung.ch/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "19607ebcf114d58905bb",
+    "url": "/jamesyoung.ch/static/js/2.d268dbf7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jamesyoung.ch/static/js/2.d268dbf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23fa0261c32a5711ee52",
+    "url": "/jamesyoung.ch/static/js/main.70eddb74.chunk.js"
+  },
+  {
+    "revision": "97938f4ebe0842f9c31b",
+    "url": "/jamesyoung.ch/static/js/runtime-main.e257e0ab.js"
+  }
+]);
